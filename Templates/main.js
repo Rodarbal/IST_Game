3ae5a64f0777e95f05b00f8d982ca4e02cc9bd3d
Rodarbal <91778305+Rodarbal@users.noreply.github.com>
2022-06-15 21:51:23 +1000

    Add files via upload

--- Templates/main.js
+++ Templates/main.js
@@ -1,1 +1,22 @@
-// JavaScript Document
+console.log("Online")
+function play() {
+    let croak = new Audio('Sounds/ribbit.mp3');
+    croak.pause();
+    croak.currentTime = 0;
+    croak.play();
+}
+/*var audio = new Audio();
+audio.src='Sounds/ribbit.mp3';
+audio.oncanplaythrough = (event) => {
+    var playedPromise = audio.play();
+    if (playedPromise) {
+        playedPromise.catch((e) => {
+             console.log(e)
+             if (e.name === 'NotAllowedError' || e.name === 'NotSupportedError') { 
+                   console.log(e.name);
+              }
+         }).then(() => {
+              console.log("playing sound !!!");
+         });
+    }
+}*/
