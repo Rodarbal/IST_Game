4296948b955bdd5cc0600f56647e9938f86d4b67
Rodarbal <91778305+Rodarbal@users.noreply.github.com>
2022-06-22 19:57:15 +1000

    Update main.js

--- main.js
+++ main.js
@@ -26,6 +26,7 @@ function chooseDialogue(option=0, cass) {
      }
 } 
 
+//test function
 function letterMaker() {
     letter++;
     let letterz = {"letters" : {"1":"a", "2":"b", "3":"c", "4":"d", "5":"e", "6":"f", "7":"g","8":"h","9":"i","10":"j","11":"k","12":"l","13":"m"}};
